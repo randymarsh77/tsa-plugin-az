c02745cd2bef528d8867e721a42a48f394760655
randymarsh77 <mattpsbs@gmail.com>
2023-03-10 10:21:35 -0800

    feat(plugin): Hello.

---
+++ README.md
@@ -0,0 +1,31 @@
+# tsa-plugin-az
+
+Azure plugin for [tsa](https://github.com/randymarsh77/tsa-cli).
+
+## Installation
+
+```
+npm i -g @tsa-tools/cli tsa-plugin-az
+```
+
+This tool instruments `az` and will use all the defaults configured by `az`.
+
+## Usage
+
+Defaults to CPU stats and matches all VM resources according to `az` defaults (subscription, resource-group).
+```
+tsa --plugin tsa-plugin-az
+```
+
+### Plugin specific options
+| Flag   | Values |
+| ------ | ------ |
+| `--metric` | One of `cpu` or `ram` |
+| `--resource-group` | forwards to `az` |
+| `--filter` | regex filter on resource name |
+
+### Standard `tsa` options
+| Flag     | Values |
+| -------- | -------- |
+| `--since` | [timestring](https://www.npmjs.com/package/timestring) relative to `now` or `--until`, when to start the query time range |
+| `--until` | [timestring](https://www.npmjs.com/package/timestring) relative to `now`, when to end the query time range |
---
+++ src/index.ts
@@ -0,0 +1,165 @@
+import { IPluginWithOptions } from '@simple-cli/base';
+import { ILabeledTimeSeriesData, ITSAPluginArgs, TSAPluginResult } from '@tsa-tools/cli';
+import { exec } from 'shelljs';
+
+enum MetricType {
+	cpu = 'cpu',
+	ram = 'ram',
+}
+
+const definitions = [
+	{
+		name: 'resource-group',
+		type: String,
+		description: 'Passed through to `az resource list`',
+	},
+	{
+		name: 'metric',
+		type: String,
+		description: 'One of [cpu|ram]',
+	},
+	{
+		name: 'filter',
+		type: String,
+		description: 'RegEx to match resource names',
+	},
+];
+
+interface IAZOptions {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	'resource-group': string;
+}
+
+interface IAdditionalOptions {
+	metric: MetricType;
+	filter: string;
+}
+
+type IAZPluginOptions = IAZOptions & IAdditionalOptions;
+
+interface IAZResource {
+	id: string;
+	name: string;
+}
+
+interface IMetricDataPoint {
+	average: number;
+	timeStamp: string;
+}
+
+const metricNames = {
+	cpu: 'Percentage CPU',
+	ram: 'Available Memory Bytes',
+};
+
+function execOrFail(command: string) {
+	const { code, stdout, stderr } = exec(command.trim(), { silent: true });
+	if (code !== 0) {
+		console.error(`❌ Command failed: '${command}' ❌`);
+		console.log(stdout);
+		console.error(stderr);
+		process.exit(1);
+	}
+
+	return stdout;
+}
+
+function az<T>(command: string) {
+	return JSON.parse(execOrFail(`az ${command}`)) as T;
+}
+
+function getVMIds(options: IAZPluginOptions) {
+	const group = options['resource-group'] ? `--resource-group ${options['resource-group']}` : '';
+	const type = '--resource-type Microsoft.Compute/virtualMachines';
+	return az<IAZResource[]>(`resource list ${type} ${group}`)
+		.filter((x) => {
+			if (!options.filter) {
+				return true;
+			}
+			const pattern = new RegExp(options.filter);
+			return pattern.test(x.name);
+		})
+		.map(({ id, name }) => ({ id, name }));
+}
+
+function getTimeSeries(id: string, options: string) {
+	const resource = `--resource ${id}`;
+	const data = az<any>(`monitor metrics list ${resource} ${options}`);
+	const ts = data.value[0].timeseries[0].data;
+	return ts as IMetricDataPoint[];
+}
+
+function transformValue(v: number, metric: MetricType) {
+	if (metric === MetricType.ram) {
+		// Available bytes, transform to GB
+		return v / 1_000_000_000;
+	}
+
+	return v;
+}
+
+function logMetricTypeDescription(metric: MetricType) {
+	switch (metric) {
+		case MetricType.cpu:
+			console.log('  CPU metrics are "Percent CPU used."');
+			console.log("  A high maximum means there isn't a lot of headroom during peak load.");
+			console.log(
+				'  A low mean means the vm might be over provisioned and is costing more money than it needs to be.'
+			);
+			break;
+		case MetricType.ram:
+			console.log('  RAM metrics are "Available GB".');
+			console.log("  A low minimum means there isn't a lot of headroom during peak load.");
+			console.log(
+				'  A high mean means the vm might be over provisioned and is costing more money than it needs to be.'
+			);
+			break;
+		default:
+			break;
+	}
+}
+
+const execute = async ({ start, end, step }: ITSAPluginArgs, options: IAZPluginOptions) => {
+	const startDate = new Date(start);
+	const endDate = new Date(end);
+	const intervalMinutes = Math.round(step / 60000);
+	const startTime = `--start-time ${startDate.toISOString()}`;
+	const endTime = `--end-time ${endDate.toISOString()}`;
+	const interval = `--interval ${intervalMinutes}m`;
+	const metricType = options.metric ?? MetricType.cpu;
+	const metrics = `--metric "${metricNames[metricType]}"`;
+	const timeSeriesFlags = `${metrics} ${startTime} ${endTime} ${interval}`;
+
+	console.log(
+		`Querying ${metricType} stats from ${startDate.toDateString()} @ ${startDate.toLocaleTimeString()} to ${endDate.toDateString()} @ ${endDate.toLocaleTimeString()} using an interval of ${intervalMinutes}m`
+	);
+	logMetricTypeDescription(metricType);
+
+	try {
+		const vmIds = getVMIds(options);
+		process.stdout.write('Fetching data...');
+		const data = vmIds.reduce<ILabeledTimeSeriesData>((acc, vm, i) => {
+			process.stdout.clearLine(0);
+			process.stdout.cursorTo(0);
+			process.stdout.write(`Fetching data for ${vm.name} ${i} / ${vmIds.length}\r`);
+			const data = getTimeSeries(vm.id, timeSeriesFlags);
+			const label = vm.name;
+			const series = data
+				.filter((x) => x.average !== null)
+				.map((x) => [new Date(x.timeStamp), transformValue(x.average, metricType)]);
+			acc[label] = series as any;
+			return acc;
+		}, {});
+		return { data };
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+
+const plugin: IPluginWithOptions<IAZPluginOptions, ITSAPluginArgs, TSAPluginResult> = {
+	definitions,
+	execute,
+};
+
+export default plugin;
